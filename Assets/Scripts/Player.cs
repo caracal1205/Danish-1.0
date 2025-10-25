62ff1feb5483e934192b4f02ddc530a7305a1458
Truong_Timothy <91552132+Bistouflere@users.noreply.github.com>
2025-10-25 16:47:29 +0200

    Create Player.cs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+public class Player
+{
+    public string Name;
+    public List<Card> hand = new List<Card>();
+    public List<Card> visible = new List<Card>();
+    public List<Card> hidden = new List<Card>();
+
+    public Player(string name) => Name = name;
+
+    public bool HasNoCards => hand.Count == 0 && visible.Count == 0 && hidden.Count == 0;
+
+    public Card DrawHidden()
+    {
+        if (hidden.Count == 0) return default;
+        var c = hidden[0];
+        hidden.RemoveAt(0);
+        return c;
+    }
+
+    public void AddToHand(List<Card> cards)
+    {
+        hand.AddRange(cards);
+    }
+
+    public void AddToHand(Card c)
+    {
+        hand.Add(c);
+    }
+}
