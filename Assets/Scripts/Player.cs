5facd0c97176cdf304743eb303e4e1ecee4ee6da
Truong_Timothy <91552132+Bistouflere@users.noreply.github.com>
2025-10-26 00:48:10 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,32 @@
-// Player.cs
 using System.Collections.Generic;
 
-[System.Serializable]
 public class Player
 {
-    // correspond à player.name et aux listes hand, visible, hidden
-    public string name;
+    public string Name;
     public List<Card> hand = new List<Card>();
     public List<Card> visible = new List<Card>();
     public List<Card> hidden = new List<Card>();
 
-    public Player(string name)
-    {
-        this.name = name;
-    }
+    public Player(string name) => Name = name;
 
     public bool HasNoCards => hand.Count == 0 && visible.Count == 0 && hidden.Count == 0;
+
+    /* (Méthodes commentées de la version originale laissées ici)
+    public Card DrawHidden()
+    {
+        if (hidden.Count == 0) return default;
+        var c = hidden[0];
+        hidden.RemoveAt(0);
+        return c;
+    }
+
+    public void AddToHand(List<Card> cards)
+    {
+        hand.AddRange(cards);
+    }
+
+    public void AddToHand(Card c)
+    {
+        hand.Add(c);
+    }*/
 }
