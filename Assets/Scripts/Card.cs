e839db96526b8c433f353366ec5540b2eaa24c85
Truong_Timothy <91552132+Bistouflere@users.noreply.github.com>
2025-10-26 00:47:15 +0200

    Update Card.cs

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,36 +1,30 @@
 using System;
 
 [Serializable]
-public class Card
+public struct Card
 {
-    public int value;  // 2..14
-    public Suit suit;  // type enum
+    public Suit suit;
+    public Rank rank;
 
-    public Card(int value, Suit suit)
+    public Card(Suit suit, Rank rank)
     {
-        this.value = value;
         this.suit = suit;
+        this.rank = rank;
     }
 
     public override string ToString()
     {
-        string rankStr;
-        switch (value)
-        {
-            case 11: rankStr = "J"; break;
-            case 12: rankStr = "Q"; break;
-            case 13: rankStr = "K"; break;
-            case 14: rankStr = "A"; break;
-            default: rankStr = value.ToString(); break;
-        }
-        return $"{rankStr} of {suit}";
+        // Affiche la carte au format "Rank de Suit"
+        return $"{rank} de {suit}";
     }
 }
 
 public enum Suit
 {
-    Hearts,    // ♥
-    Diamonds,  // ♦
-    Clubs,     // ♣
-    Spades     // ♠
+    Clubs, Diamonds, Hearts, Spades
+}
+
+public enum Rank
+{
+    Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13, Ace = 14
 }
