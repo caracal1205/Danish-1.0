691169920944a7166a1a71151b3f660895901379
Truong_Timothy <91552132+Bistouflere@users.noreply.github.com>
2025-10-25 16:47:06 +0200

    Create Card.cs

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+using System;
+
+[Serializable]
+public struct Card
+{
+    public Suit suit;
+    public Rank rank;
+
+    public Card(Suit suit, Rank rank)
+    {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public override string ToString()
+}
+
+public enum Suit
+{
+    Clubs, Diamonds, Hearts, Spades
+}
+
+public enum Rank
+{
+    Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, Queen = 12, King = 13, Ace = 14
+}
